934002f568452b8a8d6af2128ea7c06ee16d3134
TwoShoeStrings <pokiscool2@gmail.com>
2025-07-27 18:21:28 -0400

    CH2.4

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-def main():
-    contents = get_book_text("./books/frankenstein.txt")
-    print(contents)
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
 
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
 
 main()
     
